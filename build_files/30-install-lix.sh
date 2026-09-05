1be57d947f4598e7b223c2cb91dce09f2fd08e99
AshlynOrSomethin <AshlynOrSomethin@users.noreply.github.com>
2026-09-05 16:57:41 -0500

    fix: install Lix on first boot

--- build_files/30-install-lix.sh
+++ build_files/30-install-lix.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-curl -sSfL https://install.lix.systems/lix | sh -s -- \
-    install ostree \
-    --no-confirm \
-    --no-start-daemon \
-    --no-modify-profile \
-    --enable-flakes
+systemctl enable lix-install.service
